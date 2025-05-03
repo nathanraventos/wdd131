fca3ef453c6f07e671aff303b6ffa63051920cbd
Nathan Raventos <rav21003@byui.edu>
2025-05-03 15:08:53 -0600

    added function in js that starts the page in light mode to fix bug

--- mission/mission.js
+++ mission/mission.js
@@ -1,5 +1,13 @@
 const themeSelector = document.querySelector("select");
 
+function initializeTheme() {
+    if (document.body.classList.contains("dark")) {
+        themeSelector.value = "dark"; // Set the selector to dark on initial load
+    } else {
+        themeSelector.value = "light"; // Set the selector to light on initial load
+    }
+}
+
 function changeTheme() {
 
     if (themeSelector.value === "dark")
@@ -13,4 +21,7 @@ function changeTheme() {
         document.querySelector(".logo").src = "byui_logo_blue.webp";
     }
 }
+
+initializeTheme();
+
 themeSelector.addEventListener("change", changeTheme);
